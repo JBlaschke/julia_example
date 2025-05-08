2be154ff1cf179de01b3d15e3ff1104256a09963
Johannes Blaschke <jpblaschke@lbl.gov>
2025-05-08 09:50:57 -0700

    asdfsd

--- example/src/example.jl
+++ example/src/example.jl
@@ -1,5 +1,17 @@
 module example
 
+using MPI
+
 greet() = print("Hello World!")
+
+function MPI_hello()
+    MPI.Init()
+    
+    cw = MPI.COMM_WORLD
+    rank = MPI.rank(cw)
+
+    return rank
+end
+
 
 end # module example
